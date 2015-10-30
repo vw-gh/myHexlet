91fc292db1c0ae0fd628a8fc3f6ab591bce1a78d
Vitaly <vw.kiev@gmail.com>
2015-10-30 15:46:25 +0200

    module 4.Lesson 1

--- src/model/Field.java
+++ src/model/Field.java
@@ -25,16 +25,56 @@ public class Field {
     }
 
     public String getFigure(final Point point) {
-        // BEGIN (write your solution here)
 
-        // END
+        if (point.x == 0 && point.y == 0)
+            return f00;
+        if (point.x == 1 && point.y == 0)
+            return f10;
+        if (point.x == 2 && point.y == 0)
+            return f20;
+
+        if (point.x == 0 && point.y == 1)
+            return f01;
+        if (point.x == 1 && point.y == 1)
+            return f11;
+        if (point.x == 2 && point.y == 1)
+            return f21;
+
+        if (point.x == 0 && point.y == 2)
+            return f02;
+        if (point.x == 1 && point.y == 2)
+            return f12;
+        if (point.x == 2 && point.y == 2)
+            return f22;
+
+        return "";
+
 
     }
 
     public void setFigure(final Point point, final String figure) {
-        // BEGIN (write your solution here)
 
-        // END
+        if (point.x == 0 && point.y == 0)
+            f00 = figure;
+        if (point.x == 1 && point.y == 0)
+            f10 = figure;
+        if (point.x == 2 && point.y == 0)
+            f20 = figure;
+
+        if (point.x == 0 && point.y == 1)
+            f01 = figure;
+        if (point.x == 1 && point.y == 1)
+            f11 = figure;
+        if (point.x == 2 && point.y == 1)
+            f21 = figure;
+
+        if (point.x == 0 && point.y == 2)
+            f02 = figure;
+        if (point.x == 1 && point.y == 2)
+            f12 = figure;
+        if (point.x == 2 && point.y == 2)
+            f22 = figure;
+
 
     }
 
